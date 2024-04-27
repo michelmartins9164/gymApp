e969ad34d0551e741c265a5bd1aed2bc8b0152ee
Eduardo <eduardo.fgm.neto@gmail.com>
2024-04-27 15:13:42 -0300

    feat: herobox adicionado

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,16 +7,12 @@ export default function Header() {
     'FALE CONOSCO'
   ]
   return (
-    <nav className="flex items-center px-24 bg-black text-white h-20 font-bebas  ">
-      {/* falta o valor exato do padding */}
+    <nav className="flex items-center px-24 text-white h-20 font-bebas">
       <span className="flex items-center space-x-60 ">
-        {/* não consegui colocar o valor exato na Height ainda nem o valor exato do espaço entre os itens */}
         <h1 className="text-3xl">
-          IRON<span className="text-red-600">GYM</span>
+          IRON<b className="text-scarlet">GYM</b>
         </h1>
-        {/* falta o valor exato da fonte */}
         <span className="flex space-x-10 text-xl">
-          {/* falta o valor exato da fonte */}
           {NavRoutes.map((nav) => {
             console.log(nav)
             return <p key={nav}> {nav} </p>
@@ -24,11 +20,10 @@ export default function Header() {
         </span>
         <a
           href="#"
-          className=" flex items-center px-8 h-14 w-40 bg-red-600 text-xl"
+          className=" flex items-center justify-center h-14 w-40 bg-scarlet text-xl"
         >
           COMEÇE AGORA
         </a>
-        {/* falta o valor exato na Widith da ancora e no tamanho do texto */}
       </span>
     </nav>
   )
